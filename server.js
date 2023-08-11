6da4fb40858a653a597270516da1d896feab0117
Manojparm <123813997+Manojparm@users.noreply.github.com>
2023-08-11 13:03:54 +0530

    Basic server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+const app=express()
+dotenv.config()
+
+const PORT=process.env.PORT
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Welcome to My ecounsulling App</h1>")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at port http://localhost:${PORT}`)
+})
+
