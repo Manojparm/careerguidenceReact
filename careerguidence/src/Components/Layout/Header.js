4e68bcd09d56d3f6a15b834ac83790dbb6e54b1e
Manojparm <123813997+Manojparm@users.noreply.github.com>
2023-08-31 09:55:47 +0530

    responsive navbar

--- careerguidence/src/Components/Layout/Header.js
+++ careerguidence/src/Components/Layout/Header.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 const Header = () => {
+   function openmenu(e){
+    let sidemenu=document.getElementById("sidemenu");
+    e=sidemenu;
+     e.style.right="0";
+   }
+   function closemenu(e){
+    let sidemenu=document.getElementById("sidemenu");
+     e=sidemenu;
+    e.style.right="-600px";
+   }
   return (
-     <div>
-      
-     </div>
+    <div className='nav-container'>
+    <nav>
+     <h4>Career-<span>Guidence</span></h4>
+     <ul id="sidemenu">
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/signup">Signup</NavLink></li>
+      <li><NavLink to="/studentlogin">Login</NavLink></li>
+     <i className="fas fa-times" onClick={(e)=>closemenu(e)}></i>
+     </ul>
+     <i className="fas fa-bars" onClick={(e)=>openmenu(e)}></i>
+    </nav>
+    </div>
   )
 }
 
